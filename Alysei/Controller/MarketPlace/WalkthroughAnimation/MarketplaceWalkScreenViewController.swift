446418cb76240032d7d9179e872ecee13e5bc8f2
Shalini Yadav <shalini.yadav@ibyteinfomatics.in>
2021-10-27 17:59:15 +0530

    Marketplace Walkthrough

---
+++ Alysei/Controller/MarketPlace/WalkthroughAnimation/MarketplaceWalkScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceWalkScreenViewController.swift
+//  Alysei
+//
+//  Created by Gitesh Dang on 10/27/21.
+//
+
+import UIKit
+
+class MarketplaceWalkScreenViewController: AlysieBaseViewC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
